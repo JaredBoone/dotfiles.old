4f32a36c8bead2442489986809bdcf206275fb3e
Jared Boone <jared.boone@gmail.com>
2018-09-24 23:17:45 -0700

    defaults

--- install/iterm.sh
+++ install/iterm.sh
@@ -3,6 +3,12 @@
 echo "[iterm.sh] - Begin"
 
 # Install the Solarized Dark theme for iTerm
-open "${HOME}/dotfiles/iterm/themes/Solarized Dark - Patched.itermcolors"
+# Redundant - already in com.googlecode.iterm2.plist.
+# open "${HOME}/dotfiles/iterm/themes/Solarized Dark - Patched.itermcolors"
+
+# Specify the preferences directory
+defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/dotfiles/iterm"
+# Tell iTerm2 to use the custom preferences in the directory
+defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
 
 echo "[iterm.sh] - End"
