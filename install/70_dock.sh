3a668542cc465f972c872e6d8482ba1df311e9ca
Jared Boone <jared.boone@gmail.com>
2018-09-29 09:56:39 -0700

    echo

--- install/70_dock.sh
+++ install/70_dock.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "[dock.sh] - Begin"
+
 # Remove all apps from the dock
 dockutil --list | awk -F"\t" '{print $1}' | while read -r line; do
   dockutil --remove "$line"
@@ -10,3 +12,5 @@ sleep 5
 
 # Add Applications folder as a list
 dockutil --add '/Applications' --view list --display folder --sort name
+
+echo "[dock.sh] - End"
